f1167193624163111fe0f7e8f9e6597110ba60ae
Nadeem <ndmkhan068@gmail.com>
2018-03-30 11:50:24 -0400

    This is for Heredoc leading whitespace in user data Fix

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
-provider "aws" {
-region = "us-west-2"
-}
-resource "aws_instance" "example" {
-ami = "ami-d874e0a0"
+resource "aws_instance" "app" {
+ami = "ami-66ef751e"
 instance_type = "t2.micro"
 
+user_data = <<EOF
+#!/bin/bash
+echo "Hello, World" > index.html
+nohup busybox httpd -f -p 8080 &
+EOF
 tags {
-Name = "terraform-example"
+Name = "terraform example"
 }}
